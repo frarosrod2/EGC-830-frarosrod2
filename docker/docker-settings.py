2586b8141ed7ab3aeca04c30e8938fab65826d26
frarosrod2 <frarosrod2@alum.us.es>
2021-02-11 13:19:41 +0100

    feature/master Docker ready

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,10 +5,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
-}
 
 STATIC_ROOT = '/app/static/'
 MEDIA_ROOT = '/app/static/media/'
